3069a4a2b4582e84dd31c98e717b2dad07749853
eqphp <eqphp_framework@126.com>
2016-01-06 17:20:11 +0800

    add mail config

---
+++ config/local/mail.php
@@ -0,0 +1,14 @@
+<?php
+return array(
+	 'host'=>'smtp.163.com',
+	 'port'=>25,
+	 'user'=>'',
+	 'password'=>'',
+	 'from'=>array(
+		'address'=>'***@163.com',
+		'name'=>'EQPHP开源框架',
+	 ),
+	 'log'=>array(
+		'is_record_mail'=>true,
+	  ),
+ );
